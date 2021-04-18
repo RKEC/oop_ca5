a9c405f642be2e53417aed40dd9c54ff0e459d5a
Richard <d00230925@student.dkit.ie>
2021-04-18 18:14:15 +0100

    Added extra constants and deleted unused files

--- src/main/java/com/dkit/oopca5/Constants/CAOService.java
+++ src/main/java/com/dkit/oopca5/Constants/CAOService.java
@@ -1,4 +1,4 @@
-package com.dkit.oopca5.core;
+package com.dkit.oopca5.Constants;
 
 /* The CAOService class has constants to define all of the messages that are sent between the Client and Server
  */
@@ -10,7 +10,7 @@ package com.dkit.oopca5.core;
 
 public class CAOService
 {
-    public static final int PORT_NUM = 50025;
+    public static final int PORT_NUM = 8080;
     public static final String HOSTNAME = "localhost";
 
     public static final String BREAKING_CHARACTER = "%%";
@@ -18,4 +18,12 @@ public class CAOService
     public static final String REGISTER_COMMAND = "REGISTER";
     public static final String SUCCESSFUL_REGISTER = "REGISTERED";
     public static final String FAILED_REGISTER = "REG FAILED";
+    public static final String LOGIN_COMMAND = "LOGIN";
+    public static final String SUCCESSFUL_LOGIN = "LOGGED IN";
+    public static final String FAILED_LOGIN = "LOGIN FAILED";
+    public static final String CAO_NUM = "^[0-9]{8}$" ;
+    public static final String DOB = "^\\d{4}-\\d{2}-\\d{2}$" ;
+    public static final String PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{8,}$" ;
+
+
 }
